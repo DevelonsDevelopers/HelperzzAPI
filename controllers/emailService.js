fdd79ffdd827d33757d5d97af7f515a91dc15675
Tayyab Mazhar <tayyabrana213@gmail.com>
2024-04-16 17:04:20 +0500

    Email Automation

--- controllers/emailService.js
+++ controllers/emailService.js
@@ -3,8 +3,10 @@ const ServiceRequest = require("../models/serviceRequest")
 const {template} = require("../util/template");
 
 exports.contractorEmail = async (req, res, next) => {
+    console.log(req.body.email)
         let email = req.body.email;
         const nodemailer = require('nodemailer');
+        console.log("Mailer")
         let transporter = nodemailer.createTransport({
             host: 'smtp-relay.brevo.com',
             port: 587,
@@ -13,6 +15,7 @@ exports.contractorEmail = async (req, res, next) => {
                 pass: "OaNLG5CP2HMyV8IA"
             }
         })
+    console.log(transporter)
         message = {
             from: "tayyabrana5999@gmail.com",
             // to: "tayyabrana5999@gmail.com, bimalntb@gmail.com, abubakar.4983763@gmail.com",
@@ -21,7 +24,8 @@ exports.contractorEmail = async (req, res, next) => {
             text: "You have a new Job Invitation",
             html: template({ name: "Bridges Inc.", customer: "David Brown", job: "Need to Repair Roof", details: "My roof is badly damaged by some aliens from space directly landed on my roof" })
         }
-        transporter.sendMail(message, function (err, info) {
+        console.log("WOW")
+        await transporter.sendMail(message, function (err, info) {
             if (err) {
                 res.status(200).json({"responseCode": 205, "message": "Email Failed!"});
             } else {
