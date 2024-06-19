8c4c9f70f15bad4881409224c5ec53a0f7d7a991
Tayyab Mazhar <tayyabrana213@gmail.com>
2024-06-19 19:56:31 +0500

    Single

--- routes/contractorReviews.js
+++ routes/contractorReviews.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const controller = require('../controllers/contractorReviews')
 
 router.get('/all', controller.getAllReviews)
+router.get('/single/:id', controller.getReview)
 router.get('/category/:category', controller.getByCategory)
 router.put('/approve/:id', controller.approveReview)
 router.put('/reject/:id', controller.rejectReview)
